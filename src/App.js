0688fc4f1c435df823153868d5dda8c5abdf2969
Fabian Pena <camilo.fabis@gmail.com>
2020-09-17 15:46:24 -0400

    complete lab

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import logo from './logo.svg';
 import './App.css';
 import contactsJSON from './contacts.json'
 console.log(contactsJSON.length)
+
 function App() {
 
   let [ contacts, setContacts] = useState(contactsJSON.splice(0, 5));
@@ -12,12 +13,15 @@ function App() {
   return (
     <div className="App">
       <h1>IronContacts</h1>
-      <button onClick={getRandom} >Add Random Famous</button>
+      <button onClick={getRandom} >Add Random Famous</button> 
+      <button onClick={sortName} >Sort by Name</button> 
+      <button onClick={sortPopulatiry} >Sort by Popularity</button>
       <table>
             <tr>
               <th>Picture</th>
               <th>Name</th>
               <th>Popularity</th>
+              <th>Action</th>
             </tr>
       <FamousPeople/>
       </table>   
@@ -27,7 +31,7 @@ function App() {
     console.log(contacts)
     return contacts.map((eachFamousPerson) => {
       return (
-          <tr key={eachFamousPerson.name} style={{margin: '0 auto', padding: '20px'}}>
+          <tr key={eachFamousPerson.id} style={{margin: '0 auto', padding: '20px'}}>
               <td>
                 <img style={{width: '50px'}} src={eachFamousPerson.pictureUrl} alt="Holywood" />
               </td>
@@ -37,8 +41,9 @@ function App() {
               <td>
                 <p>{eachFamousPerson.popularity.toFixed(2)}</p>
               </td>
-
-
+              <td>
+                <p><button id={eachFamousPerson.id} onClick={deleteContact}>Delete</button></p>
+              </td>
         </tr>
       )
     })
@@ -46,12 +51,12 @@ function App() {
 
 
   function getRandom(){
-    let num = Math.floor(Math.random()*restOfContacts.length)
+    let num = Math.floor(Math.random() * restOfContacts.length)
     let randomContact = restOfContacts[num]
-
+    
     let newRestOfContacts = [...restOfContacts]
     let newContacts = [...contacts]
-
+    
     newRestOfContacts.splice(num,1)
     newContacts.push(randomContact)
 
@@ -59,10 +64,24 @@ function App() {
     setRestOfContacts(newRestOfContacts)
 
   }
+
+  function sortName() {
+    let newContacts = [...contacts].sort((a, b) => a.name.localeCompare(b.name));
+    setContacts(newContacts)
+    FamousPeople()
+  }
+
+  function sortPopulatiry() {
+    let newContacts = [...contacts].sort((a, b) => b.popularity - a.popularity);
+    setContacts(newContacts)
+    FamousPeople()
+  }
+
+  function deleteContact(e) {
+    let removeContacts = [...contacts].filter((contact) => contact.id !== e.target.id)
+    setContacts(removeContacts)
+  }
 }
-
-
-
 
 
 
